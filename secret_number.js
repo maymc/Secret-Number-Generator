ae6e1910eb653d945a15d88ef432baf99c3d2458
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-07 15:15:36 -1000

    completed exercise

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function (){
+
+    //declare num outside of the function so it runs once. The return will reference the global variable if it doesn't have a local one.
+    const num = Math.floor(Math.random() * 1000000);
+    return function secretNumber(){
+        console.log(num);
+        return num;
+    }
 };
+
